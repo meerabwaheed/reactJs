f62677021f56920d78c692426b0567154d8a52b5
Meerab Waheed <devmeerab@gmail.com>
2026-09-11 20:35:13 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+// function hello (){
+//     console.log ("hello");
+// }
+// setTimeout(hello,2000)//2000 = 2 second
+console.log("one");
+console.log("two");
+setTimeout(() => {
+    console.log("hello");
+}, 2000); // 4000 = 4 second
+console.log("three");
+console.log("four");
+// let age = 18;
+// if(age >= 18){
+//     if(age >= 21){
+//         console.log("aged");
+//     }else{
+//         console.log("middle");
+//     }
+// }else{
+//     console.log("child");
+// }
+// function sub(a, b) {
+//     return a - b;
+// }
+// function calculate(a, b, callback) {
+//     return callback(a, b);
+// }
+//     calculate(3, 2, sub);
+//     console.log(calculate(3, 2, sub));
+let a = prompt("enter first number");
+let b = prompt("enter second number");
+let c = prompt("what operation you want to perform write + for addition, - for subtraction");
+console.log("your answer is " + (c === "+" ? (parseInt(a) + parseInt(b)) : (parseInt(a) - parseInt(b))));
+
